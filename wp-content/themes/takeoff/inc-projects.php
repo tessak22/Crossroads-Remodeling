501552a0973cf3a97cb5b2dd4c59c19e6025d35b
Tessa Kriesel <me@tessak22.com>
2016-12-23 15:24:40 -0600

    Couple Tweaks to smaller screen sizes

--- wp-content/themes/takeoff/inc-projects.php
+++ wp-content/themes/takeoff/inc-projects.php
@@ -9,7 +9,7 @@
 
 		?>
 
-		<div class="project-item col-md-4 col-sm-6">
+		<div class="project-item col-lg-4 col-md-6">
 			<div class="project-content">
 				<img src="<?php echo $image; ?>" />
 		    	<h4><?php echo $name; ?></h4>
